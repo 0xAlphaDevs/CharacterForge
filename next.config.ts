c7a56c89e4a26e9cbf438d4cdafcf67df939f8d1
Harsh Tyagi <writetoharshtyagi@gmail.com>
2025-06-16 18:58:45 +0530

    chore: add pinata

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL("https://replicate.delivery/**")],
+  },
 
   transpilePackages: [
     "@tomo-inc/tomo-evm-kit",
