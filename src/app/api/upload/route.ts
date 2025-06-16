cf443be5db3887fd74901e64c7ed7cc351b3b501
Harsh Tyagi <writetoharshtyagi@gmail.com>
2025-06-16 18:59:02 +0530

    feat: add route for json upload

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { PinataSDK } from "pinata";
+
+export const pinata = new PinataSDK({
+  pinataJwt: `${process.env.PINATA_JWT}`,
+  pinataGateway: `${process.env.NEXT_PUBLIC_GATEWAY_URL}`,
+});
+
+export async function POST(request: NextRequest) {
+  try {
+    const jsonMetadata = await request.json();
+    console.log("Received JSON Metadata:", jsonMetadata);
+    const { cid } = await pinata.upload.public.json(jsonMetadata);
+
+    console.log("IPFS Upload Response CID:", cid);
+
+    return NextResponse.json({ ipfsHash: cid }, { status: 200 });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
